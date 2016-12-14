71be97c91b63d7f3a7915e086ca4db5b60526171
nenospk <suppakit.neno@gmail.com>
2016-12-14 22:27:11 +0700

    Update myjson.php

--- myjson.php
+++ myjson.php
@@ -11,6 +11,7 @@ $json[$orderId] = array("name" => $fullName, "tracking" => $trackNo);
 if(file_put_contents($file, json_encode($json))) {
   $file = "tracking.txt";
   $json = json_decode(file_get_contents($file), true);
-  echo $json["A3"] . "OK";
+  echo $json["A3"]["tracking"];
+  echo $json["A3"]["name"];
 }
 ?>
