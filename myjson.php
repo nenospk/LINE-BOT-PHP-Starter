17df9e12c8c333bb51162ac0494eafae29d8de28
nenospk <suppakit.neno@gmail.com>
2016-12-14 22:09:16 +0700

    Create myjson.php

---
+++ myjson.php
@@ -0,0 +1,13 @@
+<?php
+
+$user = "bross";
+$first = "Bob";
+$last = "Ross";
+
+$file = "tracking.txt";
+$json = json_decode(file_get_contents($file),true);
+
+$json[$user] = array("first" => $first, "last" => $last);
+
+file_put_contents($file, json_encode($json));
+?>
