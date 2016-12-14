cd34725f1e65f093fe10933020c334ad712dd0e3
nenospk <suppakit.neno@gmail.com>
2016-12-14 22:24:37 +0700

    Update myjson.php

--- myjson.php
+++ myjson.php
@@ -1,16 +1,16 @@
 <?php
-$user = "bross";
-$first = "Bob";
-$last = "Ross";
+$orderId = "A3";
+$fullName = "Suppakit";
+$trackNo = "234561";
 
 $file = "tracking.txt";
 $json = json_decode(file_get_contents($file), true);
 
-$json[$user] = array("first" => $first, "last" => $last);
+$json[$orderId] = array("name" => $fullName, "tracking" => $trackNo);
 
 if(file_put_contents($file, json_encode($json))) {
   $file = "tracking.txt";
   $json = json_decode(file_get_contents($file), true);
-  echo $json . "OK";
+  echo $json["A3"] . "OK";
 }
 ?>
