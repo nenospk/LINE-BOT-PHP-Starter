eab40686d1a536a2efcd6df4c2efaa59b2f5c771
nenospk <suppakit.neno@gmail.com>
2016-12-14 23:18:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,10 +28,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				{
 				'type' => 'text',
 				'text' => $my_respond
-				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
